dfef1cf135e1d86d160c25f61bb0dfc5236ca58f
christopherorea <c28_09_98@hotmail.com>
2025-02-01 06:13:39 -0600

    new model

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 
 import pandas as pd
-from tensorflow.keras.models import load_model
-from sklearn.decomposition import PCA
+import joblib
 from sklearn.metrics import r2_score as R2Score
 from tensorflow.random import set_seed
 
 set_seed(42) # Set seed for reproducibility
 
+# This needs to be changed with a method that pulls data from a source (Bucket, DB, DWH, etc)
+# Using a static name, can make the pulled content dynamic and always output different predictions.
 df = pd.read_csv("./datasets/blind_test_data.csv")
 
-pca = PCA(n_components=10)
-pca.fit(df)
+# This needs to be changed with a method that loads the model from a source (Repo, Bucket, linrary, etc)
+# models should not be hardcoded, but loaded dynamically when available
+model = joblib.load("./model.pkl")
 
-pca_df = pd.DataFrame(
-    pca.transform(df),
-    columns=[f"component_{i}" for i in range(0, 10)]
-)
-
-model = load_model('model.keras')
+df_original = pd.read_csv('./datasets/training_data.csv') # This is used to scale data back to original values
+df_original_no_target = df_original.drop('target', axis=1)
 
 if __name__ == '__main__':
-    prediction = model.predict(pca_df)
-    print(prediction)
-    pd.DataFrame(prediction).to_csv("./datasets/prediction.csv", index=False)
+    normalized_df=(df-df_original_no_target.min())/(df_original_no_target.max()-df_original_no_target.min())
+    prediction = model.predict(normalized_df)
+    normalized_df['target'] = prediction
+    denormalized_df = normalized_df * ( df_original.max() - df_original.min() ) + df_original.min()
+    print(denormalized_df['target'])
+    # This needs to be changed with a method that pushes data to a destination (Bucket, DB, DWH, etc)
+    # Here we can also add monitoring of the model, like logging the prediction, the model used, the time it took to make the prediction, etc.
+    denormalized_df.to_csv("./datasets/prediction.csv", index=False)
