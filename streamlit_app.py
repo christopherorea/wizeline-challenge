4f33316541ff6056c01f7b6fb8ec1fb103fd045c
christopherorea <c28_09_98@hotmail.com>
2025-02-01 06:13:30 -0600

    changes in standarization

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,29 @@
 
 import pandas as pd
-from tensorflow.keras.models import load_model
-from sklearn.decomposition import PCA
+import joblib
 from sklearn.metrics import r2_score as R2Score
-from tensorflow.random import set_seed
 import streamlit as st
 
 @st.cache_resource
 def get_model():
     # This needs to be changed with a method that loads the model from a source (Repo, Bucket, linrary, etc)
     # models should not be hardcoded, but loaded dynamically when available
-    return load_model('model.keras')
+    return joblib.load("./model.pkl")
 
 st.title('Prediction POC - Christopher Orea')
 uploaded_file = st.file_uploader("Upload a CSV file. 200 Mbs Max.", type=["csv"])
 
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    
-    pca = PCA(n_components=10)
-    pca.fit(df)
+df_original = pd.read_csv('./datasets/training_data.csv') # This is used to scale data back to original values
+df_original_no_target = df_original.drop('target', axis=1)
 
-    pca_df = pd.DataFrame(
-        pca.transform(df),
-        columns=[f"component_{i}" for i in range(0, 10)]
-    )
+if uploaded_file is not None:
     model = get_model()
-    prediction = model.predict(pca_df)
-    df['prediction'] = prediction
-    st.write(df)
+
+    df = pd.read_csv(uploaded_file)
+    normalized_df=(df-df_original_no_target.min())/(df_original_no_target.max()-df_original_no_target.min())
+    prediction = model.predict(normalized_df)
+    normalized_df['target_pred'] = prediction
+    denormalized_df = normalized_df * ( df_original.max() - df_original.min() ) + df_original.min()
+    st.write(denormalized_df)
 
 st.markdown(f"[Let's start working together](https://www.linkedin.com/in/chrisgalleta/)") 
